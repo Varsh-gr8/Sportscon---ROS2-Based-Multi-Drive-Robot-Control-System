49f6179151a66c1f0ee7e5be4b9cd401f24ea134
Varsh_01 <varshah.cm@gmail.com>
2025-07-25 17:44:43 +0530

    setup.py

--- src/mecanum_drive/setup.py
+++ src/mecanum_drive/setup.py
@@ -1,25 +1,23 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'mecanum_drive'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='skyno',
-    maintainer_email='skyno@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='your_name',
+    maintainer_email='your@email.com',
+    description='Mecanum drive with joystick and keyboard control',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'joystick_node = mecanum_drive.joystick_node:main',
+            'keyboard_node = mecanum_drive.keyboard_node:main',
+            'mecanum_node = mecanum_drive.mecanum_node:main',
         ],
     },
 )
